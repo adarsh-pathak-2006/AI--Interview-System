0812f683a25a68d5d3a679ae61a988cdc18350cc
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-04 23:23:32 +0530

    resume creation API

--- administration/urls.py
+++ administration/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from administration.views import RegisterAPI
+from administration.views import RegisterAPI, ResumeAPI, ResumeAPIDetail
 
 urlpatterns = [
     path('register/', RegisterAPI.as_view(), name='register'),
+    path('resume/', ResumeAPI.as_view(), name='resume'),
+    path('resume/<int:pk>/', ResumeAPIDetail.as_view(), name='resume_individual'),
 ]
--- administration/views.py
+++ administration/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from administration.models import Profile, Resume, Project
 from administration.serializers import UserSerializer, ProfileSerializer, ProjectSerializer, ResumeSerializer
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 
 class RegisterAPI(CreateAPIView):
@@ -10,3 +10,20 @@ class RegisterAPI(CreateAPIView):
     queryset=User.objects.all()
 
 
+class ResumeAPI(ListCreateAPIView):
+    serializer_class=ResumeSerializer
+
+    def get_queryset(self):
+        profile_object=get_object_or_404(Profile, user=self.request.user)
+        return Resume.objects.filter(profile=profile_object)
+    
+    def perform_create(self, serializer):
+        data=get_object_or_404(Profile, user=self.request.user)
+        serializer.save(profile=data)
+    
+class ResumeAPIDetail(RetrieveUpdateDestroyAPIView):
+    serializer_class=ResumeSerializer
+
+    def get_queryset(self):
+        profile_object=Profile.objects.filter(user=self.request.user)
+        return Resume.objects.filter(profile=profile_object)
